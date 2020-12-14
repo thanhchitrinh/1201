1034479a45d3ddf74af1abc6adcb3953c463acf9
hangtm <hangtm.vjsc09@gmail.com>
2020-12-14 22:16:38 +0700

    handling successful registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
-  get '/signup', to: 'users#new'
+  # get '/signup', to: 'users#new'
+
+  get '/users/', to: 'users#index'
+  get '/users/new', to: 'users#new'
+  post '/users', to: 'users#create'
+  get '/users/:id', to: 'users#show'
+  get '/users/:id/edit', to: 'users#edit'
+  patch '/users/:id/edit', to: 'users#update'
+  # resources :users
 end
